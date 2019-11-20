1430fecb28b4fed05060de7e6ae88dd68d1b8869
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2019-11-20 16:58:23 +0100

    livecode clearbit boilerplate

---
+++ src/index.js
@@ -0,0 +1,12 @@
+console.log("Hello from src/index.js!");
+
+// Clearbit API
+// Let’s play with the Enrichment feature of Clearbit and its API:
+
+// 👉 clearbit.com/docs?shell#enrichment-api
+
+// Hints
+// By default, when you click on a form’s submit button, the page will be reloaded.
+// With javascript you can prevent this default behavior by using the .preventDefault() function.
+// You can pass HTTP Headers to fetch. In clearBit:
+// fetch(url, { headers: { Authorization: Bearer sk_YOUR_API_KEY } }
